72585523b77154354dd4b5deb5ecf6d57811348a
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-02-10 22:31:49 +0100

    use path prefix for index page

--- clients/web/src/routes.js
+++ clients/web/src/routes.js
@@ -3,6 +3,7 @@ const Router = require('koa-router')
 const router = new Router()
 const slsPagesDir = '../.next/serverless/pages'
 
+router.get('/', ctx => require(slsPagesDir + '/index').render(ctx.req, ctx.res))
 router.get('*', async ctx => {
   try {
     const pathname = ctx.req._parsedUrl.pathname
