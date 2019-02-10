2c1571068280cd721d7ae278e619196b9a09d053
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-02-10 21:25:21 +0100

    add compression

--- clients/web/src/server.js
+++ clients/web/src/server.js
@@ -1,9 +1,11 @@
 const Koa = require('koa')
+const compress = require('koa-compress')
 const routes = require('./routes')
 
 const app = new Koa()
 
 app.use(routes.middleware())
+app.use(compress())
 app.use(async (ctx, next) => {
   ctx.res.statusCode = 200
   await next()
