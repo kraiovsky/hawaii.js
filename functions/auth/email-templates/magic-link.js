6b425af0928857fa7cf9c483e9497c1239a49b64
Roman Krayovskyy <roman.krayovskyy@klarna.com>
2019-06-01 07:13:13 +0200

    Add clickable link to login confirmation email

--- functions/auth/email-templates/magic-link.js
+++ functions/auth/email-templates/magic-link.js
@@ -1,5 +1,7 @@
-module.exports = token => ({
+const tokenConfirmationUrl = 'auth/confirm'
+
+module.exports = (token, webClientUrl) => ({
   subject: 'Login with one click',
   text: `Magic link: ${token}`,
-  html: `<p>Magic link: <b>${token}</b></p>`,
+  html: `<p>Click <b><a href="${webClientUrl}/${tokenConfirmationUrl}?token=${token}">here</a></b> to confirm your login or enter the following token at <a href="${webClientUrl}/${tokenConfirmationUrl}">the confirmation page</a> manually:</p><p>${token}</p>`,
 })
