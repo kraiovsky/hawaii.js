6a87570a5e7e6c1914b03bde6b9cbe47e1f79a48
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-06-01 11:03:21 +0200

    Add favicon

--- clients/web/components/Wrapper.js
+++ clients/web/components/Wrapper.js
@@ -16,6 +16,7 @@ export default ({ children }) => {
           {PROJECT_NAME} - {pageTitle}
         </title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" type="image/ico" href="/static/favicon.ico" />
       </Head>
       <NavBar />
       <div>{children}</div>
