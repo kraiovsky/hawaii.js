d9946c09522df3373f45e47a7cd1b6f833dc5665
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-06-23 19:33:33 +0200

    remove unused selector

--- clients/web/store/selectors.js
+++ clients/web/store/selectors.js
@@ -2,8 +2,6 @@ const isEmpty = require('just-is-empty')
 
 export const getIsFinished = (state, key) => state.isFinished[key]
 
-export const getIsError = (state, key) => state[key].isError
-
 export const getIsAuthenticatedUser = state => !isEmpty(state.auth.access_token)
 
 export const getIsAuthorizedUser = (state, pageScope) =>
