eaf51d97c86b12a339051e958f30dedc34f4837d
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-07-16 22:03:50 +0200

    fix tests for users rest client

--- functions/auth/src/utils/__tests__/users-rest-client.spec.js
+++ functions/auth/src/utils/__tests__/users-rest-client.spec.js
@@ -7,46 +7,67 @@ const Users = require('../users-rest-client')
 const tesCases = require('./users-rest-client.cases')
 
 const serviceToken = ''
-const usersApiUrl = 'http://localhost:5001/v1/users'
+const usersApiUrl = 'http://localhost:5001'
 
 const newUserObject = email => ({
-  created: true,
-  data: {
-    id: expect.any(String),
-    email: email,
-    name: null,
-    role: 'user',
-    avatar: null,
-    updatedAt: expect.any(String),
-    createdAt: expect.any(String),
-  },
+  id: expect.any(String),
+  email,
+  name: null,
+  role: 'user',
+  avatar: null,
+  updatedAt: expect.any(String),
+  createdAt: expect.any(String),
 })
 const oldUserObject = email => ({
-  created: false,
-  data: {
-    id: expect.any(String),
-    email: email,
-    name: expect.any(String),
-    role: 'admin',
-    avatar: null,
-    updatedAt: expect.any(String),
-    createdAt: expect.any(String),
-  },
+  id: expect.any(String),
+  email,
+  name: expect.any(String),
+  role: 'admin',
+  avatar: null,
+  updatedAt: expect.any(String),
+  createdAt: expect.any(String),
 })
 
+const requestIdHeader = 'x-request-id'
+const reqHeaders = {
+  [requestIdHeader]: '123-abc-456',
+}
+
+let ctx = {
+  response: {
+    get: jest.fn(header => ctx.headers[header]),
+  },
+}
+
 describe('create()', () => {
-  describe.each(tesCases.create.happy)('👍', async (email, resStatusCode, expected) => {
+  describe.each(tesCases.create.happy)('👍', (email, resStatusCode, expected) => {
     nock(usersApiUrl)
-      .post('/')
+      .post('/v1/users')
       .reply(
         resStatusCode,
-        mockFindByKey(mockUsersDB, 'email', email) || { ...mockNewUser, email: email }
+        mockFindByKey(mockUsersDB, 'email', email) || { ...mockNewUser, email },
+        reqHeaders
       )
+
+    ctx = {
+      ...ctx,
+      request: {
+        body: { email },
+      },
+      state: {
+        serviceToken,
+        config: { usersApiUrl, requestIdHeader },
+      },
+      headers: reqHeaders,
+    }
+
     test(`should ${expected}`, async () => {
-      const user = await Users.create({ email }, { serviceToken, config: { usersApiUrl } })
-      expect(user).toMatchObject(
+      const { statusCode, body, headers } = await Users.create(ctx)
+      expect(body).toMatchObject(
         resStatusCode === 201 ? newUserObject(email) : oldUserObject(email)
       )
+      expect(statusCode).toBe(resStatusCode)
+      expect(headers).toMatchObject(reqHeaders)
     })
   })
 })
