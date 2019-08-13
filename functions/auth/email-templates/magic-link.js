0863ee5eb56569d452c3cf48daef3269626bf6f6
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-08-13 22:11:44 +0200

    change copy for auth email

--- functions/auth/email-templates/magic-link.js
+++ functions/auth/email-templates/magic-link.js
@@ -3,5 +3,5 @@ const tokenConfirmationUrl = 'auth/confirm'
 module.exports = (token, webClientUrl) => ({
   subject: 'Login with one click',
   text: `Magic link: ${token}`,
-  html: `<p>Click <b><a href="${webClientUrl}/${tokenConfirmationUrl}?token=${token}">here</a></b> to confirm your login or enter the following token at <a href="${webClientUrl}/${tokenConfirmationUrl}">the confirmation page</a> manually:</p><p>${token}</p>`,
+  html: `<p>Click <b><a target="_blank" href="${webClientUrl}/${tokenConfirmationUrl}?token=${token}">here</a></b> to confirm your login`,
 })
