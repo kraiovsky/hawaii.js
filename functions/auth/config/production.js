210c25e62539c4319070c29601cb6673dec99114
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-08-28 22:07:30 +0200

    remove sensitive keys

--- functions/auth/config/production.js
+++ functions/auth/config/production.js
@@ -2,16 +2,16 @@ module.exports = {
   confirmTokenMaxAge: '5m',
   refreshTokenMaxAge: '30d',
   accessTokenMaxAge: '5m',
-  email: 'mail@hypefight.io',
-  usersApiUrl: 'https://e9taqa5ndk.execute-api.eu-north-1.amazonaws.com/production',
-  webClientUrl: 'http://localhost:5003',
+  email: '',
+  usersApiUrl: '',
+  webClientUrl: '',
   smtp: {
     host: 'smtp.ethereal.email',
     port: 587,
     secure: '',
     auth: {
-      user: 'nc5oh62o3vf455cy@ethereal.email',
-      pass: '2QRjWypntsPve5nXPF',
+      user: '',
+      pass: '',
     },
   },
 }
