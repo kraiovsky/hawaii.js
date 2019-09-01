1e1953ec8e1af01a550117004eb62893edcbf269
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-09-01 21:05:25 +0200

    fix user api base url in dev env

--- clients/web/config.js
+++ clients/web/config.js
@@ -1,6 +1,6 @@
 const config = {
   development: {
-    usersApiUrl: 'http://localhost:5001/users',
+    usersApiUrl: 'http://localhost:5001',
     authApiUrl: 'http://localhost:5002/auth',
     s3BucketUrl: '',
   },
