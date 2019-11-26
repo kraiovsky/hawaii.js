e719286804b6813b614215edbf645bef51160bc2
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-11-26 22:59:17 +0100

    add more shared validation schemas

--- libs/validation-schemas/schemas/shared.js
+++ libs/validation-schemas/schemas/shared.js
@@ -16,9 +16,15 @@ const QUERY_FIELDS_SCHEMA = Joi.string()
   .regex(QUERY_FIELDS_VALIDATION_REGEX)
   .allow('')
 
+const BOOLEAN_SCHEMA = Joi.boolean()
+
+const DATE_RANGE = Joi.string().regex(/^([0-2][0-9]|(3)[0-1])-(((0)[0-9])|((1)[0-2]))-\d{4}$/)
+
 module.exports = {
   URL_SCHEMA,
   UUID_SCHEMA,
   EMAIL_SCHEMA,
   QUERY_FIELDS_SCHEMA,
+  BOOLEAN_SCHEMA,
+  DATE_RANGE,
 }
